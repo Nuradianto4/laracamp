5b482ddbeac3316c73c870db26375405e6dd6b3f
Nuradianto4 <adhisopo44@gmail.com>
2022-04-14 13:29:09 +0700

    Seeder + Landing page

--- app/Models/CampBenefit.php
+++ app/Models/CampBenefit.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CampBenefits extends Model
+class CampBenefit extends Model
 {
     use HasFactory;
     protected $fillable = [
